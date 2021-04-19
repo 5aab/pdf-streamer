5177249c868e956c8329bef06e1acac8d0498487
Ish Mahajan <ishmahajanbhanu@gmail.com>
2021-04-20 00:34:04 +0530

    initial commit

--- src/main/java/com/example/spring/boot/composite/rest/FileStreamController.java
+++ src/main/java/com/example/spring/boot/composite/rest/FileStreamController.java
@@ -1,23 +1,26 @@
 package com.example.spring.boot.composite.rest;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.UUID;
 
 
 @Controller
 @AllArgsConstructor
 public class FileStreamController {
 
-    @RequestMapping(value = "downloadFile", method = RequestMethod.GET)
+    @RequestMapping(value = "downloadFile", method = RequestMethod.POST)
     public StreamingResponseBody getSteamingFile(HttpServletResponse response) throws IOException {
         response.setContentType("application/pdf");
         response.setHeader("Content-Disposition", "attachment; filename=\"demo.pdf\"");
@@ -30,4 +33,20 @@ public class FileStreamController {
             }
         };
     }
+
+    @ResponseBody
+    @PostMapping(value = "saveFile")
+    public String saveFile(){
+        RestTemplate restTemplate = new RestTemplate();
+        File file = restTemplate.execute("http://localhost:9095/downloadFile", HttpMethod.POST, null, clientHttpResponse -> {
+            File ret = File.createTempFile("report-"+ UUID.randomUUID(), ".pdf");
+            try(FileOutputStream out = new FileOutputStream(ret)) {
+                StreamUtils.copy(clientHttpResponse.getBody(), out);
+                return ret;
+            }
+        });
+        String absolutePath = file.getAbsolutePath();
+        return absolutePath;
+    }
+
 }
