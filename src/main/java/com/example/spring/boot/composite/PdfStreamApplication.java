4f8e1b8831721c75b6467012e51776fa1bd1a22d
Ish Mahajan <ishmahajanbhanu@gmail.com>
2021-04-19 23:54:42 +0530

    initial commit

---
+++ src/main/java/com/example/spring/boot/composite/PdfStreamApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring.boot.composite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PdfStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfStreamApplication.class, args);
+	}
+
+}
